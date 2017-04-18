6e29fac6c923438902d7e0b09c4f2427dd37532e
Horst Xu <271021733@qq.com>
2017-04-18 14:57:37 +0800

    add gulp watch

--- fe_dev/gulpfile.js
+++ fe_dev/gulpfile.js
@@ -1,12 +1,46 @@
 var gulp = require('gulp');
 var rjs = require('requirejs');
-var buildConfig = require('./buildconfig');
+
+var buildConfig = {
+  appDir: '.',
+  baseUrl: 'js',
+  dir: '../assets',
+  skipDirOptimize: true,
+  mainConfigFile: ['./require-config.js'],
+  modules: [
+    {
+      name: 'indexpage',
+      exclude: ['normalize'],
+      insertRequire: ["indexpage"]
+    }
+  ],
+  paths: {
+    jquery: "empty:"
+  },
+  optimizeCss: "standard",
+  removeCombined: true,
+  fileExclusionRegExp: /^(node_modules|package.json|gulpfile.js)$/,
+  writeBuildTxt: false
+};
 
 gulp.task('build', function(cb){
   rjs.optimize(buildConfig, function(buildResponse){
     console.log('build response', buildResponse);
     cb();
   }, cb);
+});
+
+gulp.task('dev', function() {
+  var watcher1 = gulp.watch(['./css/*.css'], ['build']);
+  var watcher2 = gulp.watch(['./js/**/*.js'], ['build']);
+  watcher1.on('change', function(event) {
+    console.log('CSS file ' + event.path + ' was ' + event.type +
+      ', running build tasks...');
+  });
+  watcher2.on('change', function(event) {
+    console.log('JS file ' + event.path + ' was ' + event.type +
+      ', running build tasks...');
+  });
 });
 
 gulp.task('default', ['build']);
--- fe_dev/js/page/indexpage.js
+++ fe_dev/js/page/indexpage.js
@@ -1,6 +1,6 @@
 define(['m1', 'm3', 'jquery'], function(){
-    function abc () {
-        var biuu = 1;
+    function abcd () {
+        var biuu = 12;
         return 1;
     };
     console.log('this is indexpage.js');
